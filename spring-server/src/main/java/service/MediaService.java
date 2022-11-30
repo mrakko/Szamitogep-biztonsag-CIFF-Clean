fb3d8d4c9bd910a18cba0cff786f0a0c484a0f70
Karmacsi Péter <peterkarmacsi@gmail.com>
2022-11-30 14:25:10 +0100

    Create tmp if does not exist

--- spring-server/src/main/java/service/MediaService.java
+++ spring-server/src/main/java/service/MediaService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
@@ -14,14 +15,26 @@ public class MediaService {
     private static final String TMP_GIF_FOLDER_PATH = "tmp/gif";
     
     public void addCaff(byte[] content, String name, Long userId){
-        Path caff_path = Paths.get(TMP_CAFF_FOLDER_PATH + "/" + name + ".caff");
-        Path gif_path = Paths.get(TMP_GIF_FOLDER_PATH + "/" + name + ".gif");
+        String uuid = UUID.randomUUID().toString();
+        Path caff_path = Paths.get(TMP_CAFF_FOLDER_PATH + "/" + uuid + ".caff");
+        Path gif_path = Paths.get(TMP_GIF_FOLDER_PATH + "/" + uuid + ".gif");
+        
+        try {
+            Files.createDirectories(caff_path.getParent());
+            Files.createFile(caff_path);
+            Files.createDirectories(gif_path.getParent());
+            Files.createFile(gif_path);
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating tmp folders.");
+            e.printStackTrace();
+        }
 
         try {
             Files.write(caff_path, content);
         } catch (IOException e) {
             System.out.println("An error occurred while creating tmp caff file.");
             e.printStackTrace();
+            return;
         }
 
         Runtime rt = Runtime.getRuntime();
@@ -30,6 +43,7 @@ public class MediaService {
         } catch (IOException e) {
             System.out.println("An error occurred while converting caff file.");
             e.printStackTrace();
+            return;
         }
 
         byte[] gifContent = null;
@@ -41,6 +55,14 @@ public class MediaService {
             return;
         }
         
+        try {
+            Files.delete(caff_path);
+            Files.delete(gif_path);
+        } catch (IOException e) {
+            System.out.println("An error occurred while deleting tmp files.");
+            e.printStackTrace();
+        }
+
         //TODO save to database
     }
 
