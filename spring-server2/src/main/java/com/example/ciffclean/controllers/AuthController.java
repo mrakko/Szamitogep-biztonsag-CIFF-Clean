e258122886700c0f6553fdf10822f2edffdcff08
ibcs <ifj.benko.csaba997@gmail.com>
2022-12-02 13:09:29 +0100

    changePWD and loginUser stub

--- spring-server2/src/main/java/com/example/ciffclean/controllers/AuthController.java
+++ spring-server2/src/main/java/com/example/ciffclean/controllers/AuthController.java
@@ -3,9 +3,11 @@ package com.example.ciffclean.controllers;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ciffclean.domain.AppUser;
+import com.example.ciffclean.models.ChangeUserPasswordDTO;
 import com.example.ciffclean.models.CreateUserDTO;
+import com.example.ciffclean.models.LoginUserDTO;
 import com.example.ciffclean.models.UserTokenDTO;
 import com.example.ciffclean.repositories.UserRepository;
 
@@ -35,16 +39,52 @@ public class AuthController {
             appUser.setEmail(createUserDTO.getEmail());
             appUser.setFullName(createUserDTO.getFullName());
             AppUser savedUser = userRepository.save(appUser);
-            UserTokenDTO userTokenDTO = new UserTokenDTO();
-            userTokenDTO.setUserId(savedUser.getId());
-            userTokenDTO.setValue(savedUser.getFullName()); //TODO: Mi legyen benne?
-            userTokenDTO.setExpirationDate(LocalDateTime.now().plus(Duration.of(10, ChronoUnit.MINUTES)));  //TODO: Meddig legyen érvényes?
-            return ResponseEntity.ok(userTokenDTO);
+            return ResponseEntity.ok(generateUserTokenDTO(savedUser));
 
         }catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<UserTokenDTO> loginUser(@RequestBody LoginUserDTO loginUserDTO){
+        try{
+            //TODO: Input validation
+            var userList = userRepository.findByEmail(loginUserDTO.getEmail());
+            if(userList.isEmpty()){ return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); }
+            if(userList.size() != 1){ return ResponseEntity.status(HttpStatus.CONFLICT).build(); }
+            return ResponseEntity.ok(generateUserTokenDTO(userList.get(0)));
+        }catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("changePassword")
+    public ResponseEntity<Boolean> changePassword(@RequestBody ChangeUserPasswordDTO changeUserPasswordDTO){
+        try{
+            //TODO: Input validation
+            //TODO: API leírást kiegészíteni. Kell a DTO-ba egy mező, hogy melyik User kéri a változtatást
+            Long userId = Long.valueOf(0);
+            Optional<AppUser> appUser = userRepository.findById(userId);
+            if(!appUser.isPresent()){   return ResponseEntity.status(HttpStatus.NO_CONTENT).build();}
+            if(appUser.get().getEmail().equals(changeUserPasswordDTO.getOldPassword())){ /*change PWD */} 
+            //TODO: API-t kiegészíteni: PWD!
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+        }catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private UserTokenDTO generateUserTokenDTO(AppUser appUser){
+        UserTokenDTO userTokenDTO = new UserTokenDTO();
+        userTokenDTO.setUserId(appUser.getId());
+        userTokenDTO.setValue(appUser.getFullName()); //TODO: Mi legyen benne?
+        userTokenDTO.setExpirationDate(LocalDateTime.now()
+            .plus(Duration.of(10, ChronoUnit.MINUTES)));  //TODO: Meddig legyen érvényes?
+        return userTokenDTO;
     }
     
 }
--- spring-server2/src/main/java/com/example/ciffclean/repositories/UserRepository.java
+++ spring-server2/src/main/java/com/example/ciffclean/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.ciffclean.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.ciffclean.domain.AppUser;
 
 public interface UserRepository extends CrudRepository<AppUser, Long> {
+    List<AppUser> findByEmail(String email);
 }
