9a984eeee9d86b4ffdef201ffba8f4c6e7bbedf6
ibcs <ifj.benko.csaba997@gmail.com>
2022-12-02 12:27:58 +0100

    registerUser fix

--- spring-server2/src/main/java/com/example/ciffclean/controllers/AuthController.java
+++ spring-server2/src/main/java/com/example/ciffclean/controllers/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,7 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
+    @PostMapping("register")
     public ResponseEntity<UserTokenDTO> registerUser(@RequestBody CreateUserDTO createUserDTO){
         try{
             //TODO: Input validation
