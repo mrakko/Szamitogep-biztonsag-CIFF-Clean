bccd307847105b0d33137cd39fc4fc2f7c342c1a
Brigitta Csajka <csajkusz@gmail.com>
2022-12-04 16:41:56 +0100

    delete spring server

--- spring-server/src/main/java/io/swagger/repositories/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import io.swagger.domain.Auth;
-
-public interface AuthRepository extends CrudRepository<Auth, Long> {
-    
-}
